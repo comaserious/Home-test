6376fbace5b205e7b5145d835e09663c61d2d2fa
PRO <sciboviator@gmail.com>
2024-01-31 20:49:47 +0900

    집에서 올려보기

---
+++ src/main/java/com/orgiraffers/test/Application.java
@@ -0,0 +1,10 @@
+package com.orgiraffers.test;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println("world");
+
+    }
+}
